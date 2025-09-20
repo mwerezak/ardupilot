9800a52f94a9145b7443bbc123e1badcd6f6fd41
mwerezak <242428+mwerezak@users.noreply.github.com>
2025-09-19 21:32:39 -0400

    Update VTX table for RushFPV MAX SOLO

--- libraries/AP_VideoTX/AP_VideoTX.cpp
+++ libraries/AP_VideoTX/AP_VideoTX.cpp
@@ -107,18 +107,19 @@ const uint16_t AP_VideoTX::VIDEO_CHANNELS[AP_VideoTX::MAX_BANDS][VTX_MAX_CHANNEL
 // mapping of power level to milliwatt to dbm
 // valid power levels from SmartAudio spec, the adjacent levels might be the actual values
 // so these are marked as level + 0x10 and will be switched if a dbm message proves it
+// RushFPV MAX SOLO VTX SA2.1 supported power levels: 14, 27, 30, 34 dbm
 AP_VideoTX::PowerLevel AP_VideoTX::_power_levels[VTX_MAX_POWER_LEVELS] = {
     // level, mw, dbm, dac
-    { 0xFF,  0,    0, 0    }, // only in SA 2.1
-    { 0,    25,   14, 7    },
-    { 0x11, 100,  20, 0xFF }, // only in SA 2.1
-    { 1,    200,  23, 16   },
-    { 0x12, 400,  26, 0xFF }, // only in SA 2.1
-    { 2,    500,  27, 25   },
-    { 0x12, 600,  28, 0xFF }, // Tramp lies above power levels and always returns 25/100/200/400/600
-    { 3,    800,  29, 40   },
-    { 0x13, 1000, 30, 0xFF }, // only in SA 2.1
-    { 0xFF, 0,    0,  0XFF, PowerActive::Inactive }  // slot reserved for a custom power level
+    { 0xFF,    0,    0, 0    }, // only in SA 2.1
+    { 0,      10,   14, 7    },
+    { 0x11,   25,   18, 0xFF }, // only in SA 2.1
+    { 1,      80,   23, 16   },
+    { 0x12,  200,   27, 0xFF }, // only in SA 2.1
+    { 2,     250,   28, 25   },
+    { 0x12,  400,   30, 0xFF }, // Tramp lies above power levels and always returns 25/100/200/400/600
+    { 3,     800,   33, 40   },
+    { 0x13, 1000,   34, 0xFF }, // only in SA 2.1
+    { 0xFF,    0,    0,  0XFF, PowerActive::Inactive }  // slot reserved for a custom power level
 };
 
 AP_VideoTX::AP_VideoTX()
